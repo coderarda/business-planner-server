ffe438a0d7251f373527813951644568f91a88e6
Arda Akcagöz <arda.akcagoz@outlook.com>
2023-06-19 10:52:54 +0300

    Mindsdb Progress

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { DataTypes, Sequelize } from "sequelize";
 import cors from "cors";
 import * as dotenv from "dotenv";
 import { CalendarEvent } from "../../business-planner-shared/src/CalendarEvent";
+import { connectMindsDB } from "./mdbConn";
+
 
 dotenv.config();
 
@@ -11,53 +13,14 @@ app.use(cors());
 
 app.listen(3000, () => console.log("Listening at port 3000!"));
 
-
-const sequelize = new Sequelize({
-    username: "root",
-    password: process.env.password,
-    database: process.env.database,
-    dialect: "mysql",
-});
-
-sequelize
-    .authenticate()
-    .then(() => {
-        console.log("Connected!");
-    })
-    .catch((err) => {
-        console.log("Error connecting: " + err);
-    });
-
-const Developer = sequelize.define("Developer", {
-    id:{
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-    },
-    firstName:{
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    lastName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    }
-});
-
-async () => {
-    const dev = Developer.create({ firstName: "Arda", lastName: "Akcagöz" });
-    if (dev instanceof Developer) {
-        await dev.save();
-    }
-}
-
+connectMindsDB();
 
 app.get('/', (req, res) => {
-    const obj : CalendarEvent = {
+    const obj: CalendarEvent = {
         title: "default",
         description: "Default desc",
         date: new Date().toUTCString(),
-        id:1,
+        id: 1,
     };
     res.send(JSON.stringify(obj));
 });
