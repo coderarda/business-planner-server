6894435ba9c0c763232d6961421e9cac7154c539
Arda Akcagöz <akcagozarda@outlook.com>
2023-10-27 00:00:15 +0300

    Switched to ES Modules

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import Express from "express";
-import { DataTypes, Sequelize } from "sequelize";
 import cors from "cors";
 import * as dotenv from "dotenv";
 import { CalendarEvent } from "../../business-planner-shared/src/CalendarEvent";
-import { connectMindsDB } from "./mdbConn";
 
 
 dotenv.config();
@@ -12,8 +10,6 @@ const app = Express();
 app.use(cors());
 
 app.listen(3000, () => console.log("Listening at port 3000!"));
-
-connectMindsDB();
 
 app.get('/', (req, res) => {
     const obj: CalendarEvent = {
